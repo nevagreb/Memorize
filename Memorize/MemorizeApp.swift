f221e8de8f0b0998003117d4a1384a611427b3ea
Kristina Grebneva <kristina.o,grebneva@gmail.com>
2023-11-29 19:25:09 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kristina Grebneva on 29.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
